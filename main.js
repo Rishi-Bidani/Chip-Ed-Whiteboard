3b0e700faabfdf7e7dcdfd805f459dfcbab64af4
Rishi-Bidani <64310471+Rishi-Bidani@users.noreply.github.com>
2021-02-09 22:07:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,23 +5,29 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 
+
+const io = require("socket.io-client");
+
+const socket = io('http://127.0.0.1:5000/');
+
+socket.emit('hello', "hello world");
+
 let mainWindow;
 
 // listening for the app
 
-
 app.on('ready', function(){
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, "/templates/mainWindow.html"),
+        pathname: path.join(__dirname, "templates/mainWindow.html"),
         protocol: 'file',
         slashes: true
     }));
 
     //Build menu from template
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+    // const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
     //Inseerting the menu
-    Menu.setApplicationMenu(mainMenu)
+    // Menu.setApplicationMenu(mainMenu)
 });
 
 
@@ -29,8 +35,8 @@ app.on('ready', function(){
 // Creating menu for mainWindow
 
 
-const mainMenuTemplate = [
-    {
-        label: 'File'
-    }
-]
+// const mainMenuTemplate = [
+//     {
+//         label: 'File'
+//     }
+// ]
